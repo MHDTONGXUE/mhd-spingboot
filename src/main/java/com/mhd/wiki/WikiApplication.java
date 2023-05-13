1302a9f092bd7eac2c3ac751da931e43c32973c0
mohanding <mhd_tongxue@163.com>
2023-05-13 22:40:34 +0800

    修改启动文案

--- src/main/java/com/mhd/wiki/WikiApplication.java
+++ src/main/java/com/mhd/wiki/WikiApplication.java
@@ -1,14 +1,19 @@
 package com.mhd.wiki;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class WikiApplication {
-
+    private static final Logger Log= LoggerFactory.getLogger(WikiApplication.class);
     public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(WikiApplication.class);
+        ConfigurableEnvironment env = app.run(args).getEnvironment();
+        Log.info("启动成功");
+        Log.info("地址:\t http://127.0.0.1:{}",env.getProperty("server.port"));
 
-        SpringApplication.run(WikiApplication.class, args);
 
     }
 }
