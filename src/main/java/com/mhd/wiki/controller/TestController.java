7a15fccf5f9b31e67089d08ba8f6963593429397
mohanding <mhd_tongxue@163.com>
2023-05-13 23:25:35 +0800

    增加默认配置项示例

--- src/main/java/com/mhd/wiki/controller/TestController.java
+++ src/main/java/com/mhd/wiki/controller/TestController.java
@@ -1,8 +1,9 @@
 package com.mhd.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 /**
  * @author 莫寒鼎
  * @version 1.0
@@ -10,11 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestController {
+    //自定义配置项
+    @Value("${test.hello:这是默认配置项}")
+    private String testHello;
     /*
     get,post,put,delete
      */
     @RequestMapping("/hello")
     public String hello(){
-        return "hello";
+        return "hello"+testHello;
+    }
+    @PostMapping("/hello1")
+    public String hello(String name){
+        return "hello"+name;
     }
 }
