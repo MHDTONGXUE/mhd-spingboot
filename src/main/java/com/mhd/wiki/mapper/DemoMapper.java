3965c1b8d9e8ac01359a4d03cb92e3a71f7410ec
mohanding <mhd_tongxue@163.com>
2023-05-15 15:58:10 +0800

    集成mybatis generator，生成demo表代码

---
+++ src/main/java/com/mhd/wiki/mapper/DemoMapper.java
@@ -0,0 +1,31 @@
+package com.mhd.wiki.mapper;
+
+import com.mhd.wiki.domain.Demo;
+import com.mhd.wiki.domain.DemoExample;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface DemoMapper {
+    long countByExample(DemoExample example);
+
+    int deleteByExample(DemoExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Demo row);
+
+    int insertSelective(Demo row);
+
+    List<Demo> selectByExample(DemoExample example);
+
+    Demo selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("row") Demo row, @Param("example") DemoExample example);
+
+    int updateByExample(@Param("row") Demo row, @Param("example") DemoExample example);
+
+    int updateByPrimaryKeySelective(Demo row);
+
+    int updateByPrimaryKey(Demo row);
+}
